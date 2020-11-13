3093884d8f8c98dcc00f90236a0736ee10815554
hllbr <halibrahim.kocak@gmail.com>
2020-11-13 16:39:14 +0300

    NUMPY COMMİT

---
+++ Veri Analizi-NUMPY-Bilinmesi Gerekenler.py
@@ -0,0 +1,29 @@
+"""
+
+Veri Analizi - NUMPY
+
+
+Python veri analizinde iyi bir dildir.
+
+NUMPY bir lieaner modüldür.
+
+Veri Madenciliği, Makine Öğrenmesi, Veri Analizi işlemllerinde kullanılan br MODÜL.
+
+İçeriğinde Tek Boyutlu ve Çok Boyutlu Array Yapısı bulunmaktadır.
+
+Veri anailizi yaparken neden Numpy kullanıyoruz ?
+
+Numpy içerisinde bulunan array yapısı listelere göre oldukça az yer kaplar,
+
+Arraylerin elemanlarına erişmek listelere göre daha hızlıdır.
+
+Numpy arrayleri üzerinde effektif metodlar ve fonksiyonlar kullanarak veri analizi işlemlerimizi halledebiliyoruz.
+
+numpy anacconda ile hazır gelir gelmedği taktirde :
+Anaconda Distribution varsa = conda install numpy
+Anaconda Distribution yoksa cmd ile = pip install numpy 
+ile kurulumu gerçekleştirebilirsiniz.
+
+
+
+"""
